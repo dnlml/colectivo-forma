901935e6f21b92330c3df2e1f25b2d3d15f69b82
dnlml <info@danielemeli.com>
2018-05-01 11:48:08 +0200

    Adding slider (tmp photos)

--- src/assets/scripts/parts/slideshow.js
+++ src/assets/scripts/parts/slideshow.js
@@ -1,26 +1,19 @@
-import Flickity from "flickity";
-
 class Slideshow {
   constructor() {
-    this.slideContainer = document.querySelector('.slideshow__list');
-    this.option = {
-      autoPlay: false,
-      wrapAround: true,
-      fullscreen: true,
-      pageDots: false,
-      arrowShape: {
-        x0: 10,
-        x1: 60, y1: 50,
-        x2: 70, y2: 40,
-        x3: 30
-      }
-    };
-
     this.init();
   }
 
   init() {
-    this.slideshow = new Flickity(this.slideContainer, this.option);
+    let current = 0;
+    const slides = document.querySelectorAll('.slideshow__item');
+    const slidesLength = slides.length;
+    setInterval(() => {
+      for (let i = 0; i < slidesLength; i++) {
+        slides[i].style.opacity = 0;
+      }
+      current = (current != slidesLength - 1) ? current + 1 : 0;
+      slides[current].style.opacity = 1;
+    }, 7000);
   }
 }
 
